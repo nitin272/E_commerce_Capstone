2bb8bb00568678e53da587118daaa8700c81dc5e
Nitin272 <142768664+nitin272@users.noreply.github.com>
2024-10-08 14:09:59 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 
 const allowedOrigins = [
-  'http://localhost:4500', "http://localhost:4000" ,"https://e-commerce-capstone.onrender.com",
+  'http://localhost:4500', "http://localhost:4000" ,"https://e-commerce-capstone.onrender.com","https://scalemart.netlify.app",
   // other allowed origins can be added here
 ];
 const corsOptions = {
