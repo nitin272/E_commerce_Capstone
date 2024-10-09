096b759ffa64d8d95db1fd3da0af6b28f9e98e25
Nitin272 <142768664+nitin272@users.noreply.github.com>
2024-10-09 13:38:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 
 const allowedOrigins = [
-  'http://localhost:4500', "http://localhost:4000" ,"https://e-commerce-capstone.onrender.com","https://balaji-electronic100.vercel.app","http://e-commerce-capstone.onrender.com","https://scalemart.vercel.app/",
+  'http://localhost:4500', "http://localhost:4000" ,"https://e-commerce-capstone.onrender.com","http://e-commerce-capstone.onrender.com","https://scale-mart1.vercel.app/",
   // other allowed origins can be added here
 ];
 const corsOptions = {
