a0b0c598baa8d28a1dfb0f2f289402b87e8cc066
Nitin272 <142768664+nitin272@users.noreply.github.com>
2025-02-28 11:59:57 +0530

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -411,7 +411,6 @@ const Home = () => {
           textAlign: 'center', 
           color: '#333',
           background: 'linear-gradient(to right, #3b82f6, #8b5cf6)',
-          color: 'white',
           py: 3
         }}>
           Login Required
