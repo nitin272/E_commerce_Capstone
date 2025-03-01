b0d3cf46196ca25a42226b552c06d04f9ce94360
Nitin272 <142768664+nitin272@users.noreply.github.com>
2025-03-01 14:39:49 +0530

    Update auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -62,7 +62,7 @@ auth.get('/auth/google/callback',
                 httpOnly: true,
                 secure: process.env.NODE_ENV === 'production', 
                 maxAge: 7 * 24 * 60 * 60 * 1000,
-                // sameSite: "None"
+                sameSite: "None"
             });
             res.redirect('https://scale-mart1.vercel.app/');
 
